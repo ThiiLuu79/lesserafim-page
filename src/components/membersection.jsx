780f5395da6b53da2affc11ae2433fe817993b2f
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-03-06 09:50:08 -0500

    Updated data

--- src/components/membersection.jsx
+++ src/components/membersection.jsx
@@ -1,46 +1,28 @@
 import React from 'react';
 import LeSserafimMember from './lesserafim-member';
+import membersData from '../data/membersData';
 
-import chaewonimg from '../images/members/chaewon.png';
-import sakuraimg from '../images/members/sakura.png';
-import yunjinimg from '../images/members/yunjin.png';
-import kazuhaimg from '../images/members/kazuha.png';
-import eunchaeimg from '../images/members/eunchae.png';
+import { Link } from 'react-router-dom';
 
 const MembersSection = () => {
-
   return (
     <section id="members" className="py-5" data-aos="fade-right">
-        <div className="container">
-            <h2 className="text-center mb-4">Meet the Members</h2>
-            <div className="row">
-                <LeSserafimMember
-                name="Kim Chaewon"
-                imageSrc={chaewonimg}
-                description="Kim Chaewon is a South Korean singer and leader of LE SSERAFIM. She is known for her captivating voice and leadership skills in the group."
+      <div className="container">
+        <h2 className="text-center mb-4">Meet the Members</h2>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+          {membersData.map((member) => (
+            <div key={member.id} className="col">
+              <Link to={`/members/${member.id}`} className="text-decoration-none">
+                <LeSserafimMember 
+                  name={member.name} 
+                  imageSrc={member.imageSrc} 
+                  description={member.description} 
                 />
-                <LeSserafimMember
-                name="Miyawaki Sakura"
-                imageSrc={sakuraimg}
-                description="Sakura is a Japanese singer and one of the most prominent members of LE SSERAFIM. She is known for her elegant visuals and strong stage presence."
-                />
-                <LeSserafimMember
-                name="Huh Yunjin"
-                imageSrc={yunjinimg}
-                description="Huh Yunjin is a talented vocalist known for her rich voice and emotional depth. She brings a lot of soul to LE SSERAFIM's music."
-                />
-                <LeSserafimMember
-                name="Nakamura Kazuha"
-                imageSrc={kazuhaimg}
-                description="Kazuha is a Japanese-born member known for her ballet background and sharp dance skills. Her elegance and grace shine through in her performances."
-                />
-                <LeSserafimMember
-                name="Hong Eunchae"
-                imageSrc={eunchaeimg}
-                description="Hong Eunchae is the youngest member of LE SSERAFIM, known for her bright energy and impressive vocal abilities at such a young age."
-                />
+              </Link>
             </div>
+          ))}
         </div>
+      </div>
     </section>
   );
 };
--- src/data/membersData.js
+++ src/data/membersData.js
@@ -1,9 +1,15 @@
+import chaewon from "../images/members/chaewon.png";
+import sakura from "../images/members/sakura.png";
+import yunjin from "../images/members/yunjin.png";
+import kazuha from "../images/members/kazuha.png";
+import eunchae from "../images/members/eunchae.png";
+
 const membersData = [
     {
         id: 1,
         name: "Kim Chaewon",
-        imageSrc: "",
-        description: "Leader and main vocalist of LE SSERAFIM.",
+        imageSrc: chaewon,
+        description: "Kim Chaewon is a South Korean singer and leader of LE SSERAFIM. She is known for her captivating voice and leadership skills in the group.",
         birthday: "August 1, 2000",
         position: "Leader, Vocalist, Dancer",
         nationality: "South Korean",
@@ -12,8 +18,8 @@ const membersData = [
     {
         id: 2,
         name: "Miyawaki Sakura",
-        imageSrc: "",
-        description: "Visual and vocalist of the group.",
+        imageSrc: sakura,
+        description: "Sakura is a Japanese singer and one of the most prominent members of LE SSERAFIM. She is known for her elegant visuals and strong stage presence.",
         birthday: "March 19, 1998",
         position: "Vocalist, Rapper, Dancer",
         nationality: "Japanese",
@@ -22,8 +28,8 @@ const membersData = [
     {
         id: 3,
         name: "Huh Yunjin",
-        imageSrc: "",
-        description: "Vocalist of the group.",
+        imageSrc: yunjin,
+        description: "Huh Yunjin is a talented vocalist known for her rich voice and emotional depth. She brings a lot of soul to LE SSERAFIM's music.",
         birthday: "October 8, 2001",
         position: "Vocalist, Rapper",
         nationality: "Korean-American",
@@ -32,8 +38,8 @@ const membersData = [
     {
         id: 4,
         name: "Nakamura Kazuha",
-        imageSrc: "",
-        description: "Rapper and Dancer of the group.",
+        imageSrc: kazuha,
+        description: "Kazuha is a Japanese-born member known for her ballet background and sharp dance skills. Her elegance and grace shine through in her performances.",
         birthday: "August 9, 2003",
         position: "Sub-Vocalist, Rapper, Dancer",
         nationality: "Japanese",
@@ -42,8 +48,8 @@ const membersData = [
     {
         id: 5,
         name: "Hong Heunchae",
-        imageSrc: "",
-        description: "Lead dancer of the group.",
+        imageSrc: eunchae,
+        description: "Hong Eunchae is the youngest member of LE SSERAFIM, known for her bright energy and impressive vocal abilities at such a young age.",
         birthday: "November 10, 2006",
         position: "Vocalist, Lead Dancer, Maknae",
         nationality: "South Korean",
