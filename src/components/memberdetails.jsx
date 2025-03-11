a7eec629a8f5524479b4e0d37be7b08823d94a70
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-03-11 12:21:47 -0400

    Added data

--- src/components/memberdetails.jsx
+++ src/components/memberdetails.jsx
@@ -17,10 +17,11 @@ const MemberDetails = () => {
     <div className="container mt-5 mb-5" data-aos="fade-right">
       <button className="btn btn-secondary mb-4" onClick={() => navigate("/")}>← Back to Homepage</button>
       <div className="card shadow-lg p-4">
+        <p>Informations are taken from <a href="https://kprofiles.com/le-sserafim-members-profile/" target="_blank" rel="noreferrer">LE SSERAFIM Members Profile</a> </p>
         <div className="row">
           <div className="col-md-4 text-center">
             <img 
-              src={member.imageSrc} 
+              src={member.imageSrc2} 
               alt={member.name} 
               className="img-fluid rounded shadow-sm"
               style={{ maxWidth: "100%" }}
@@ -30,9 +31,16 @@ const MemberDetails = () => {
             <h2 className="mb-3">{member.name}</h2>
             <p className="text-muted">{member.description}</p>
             <ul className="list-group list-group-flush">
-              <li className="list-group-item"><strong>Birthday:</strong> {member.birthday}</li>
+              <li className="list-group-item"><strong>Birth Name:</strong> {member.birthname}</li>
               <li className="list-group-item"><strong>Position:</strong> {member.position}</li>
               <li className="list-group-item"><strong>Nationality:</strong> {member.nationality}</li>
+              <li className="list-group-item"><strong>Birthday:</strong> {member.birthday}</li>
+              <li className="list-group-item"><strong>Zodiac:</strong> {member.zodiac}</li>
+              <li className="list-group-item"><strong>Chinese Zodiac:</strong> {member.chineseZodiac}</li>
+              <li className="list-group-item"><strong>Height:</strong> {member.height}</li>
+              <li className="list-group-item"><strong>Weight:</strong> {member.weight}</li>
+              <li className="list-group-item"><strong>Blood Type:</strong> {member.bloodType}</li>
+              <li className="list-group-item"><strong>MBTI:</strong> {member.mbti}</li>
             </ul>
             <div className="mt-3">
               <a
