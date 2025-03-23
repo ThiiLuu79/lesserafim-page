42d0fac446c87e830d5c1ab0a507552142bd5f73
Tien Chung <tchung7171@gmail.com>
2025-03-23 12:32:32 -0400

    Created Discography Data

---
+++ src/data/discographyData.js
@@ -0,0 +1,307 @@
+export const discographyData = [
+  {
+    "title": "FEARLESS",
+    "type": "1st Mini Album",
+    "release_date": "2022-05-02",
+    "songs": [
+      "The World Is My Oyster",
+      "FEARLESS",
+      "Blue Flame",
+      "The Great Mermaid",
+      "Sour Grapes"
+    ]
+  },
+  {
+    "title": "ANTIFRAGILE",
+    "type": "2nd Mini Album",
+    "release_date": "2022-10-17",
+    "songs": [
+      "The Hydra",
+      "ANTIFRAGILE",
+      "Impurities",
+      "No Celestial",
+      "Good Parts (when the quality is bad but I am)"
+    ]
+  },
+  {
+    "title": "FEARLESS (Japanese ver.)",
+    "type": "1st Japanese Single Album",
+    "release_date": "2023-01-25",
+    "songs": [
+      "FEARLESS (Japanese ver.)",
+      "Blue Flame (Japanese ver.)",
+      "Choices"
+    ]
+  },
+  {
+    "title": "UNFORGIVEN",
+    "type": "1st Studio Album",
+    "release_date": "2023-05-01",
+    "songs": [
+      "The World Is My Oyster (2023 Ver.)",
+      "FEARLESS (2023 Ver.)",
+      "Blue Flame (2023 Ver.)",
+      "The Hydra",
+      "ANTIFRAGILE",
+      "Impurities",
+      "Burn the Bridge",
+      "UNFORGIVEN (feat. Nile Rodgers)",
+      "No-Return (Into the unknown)",
+      "Eve, Psyche & The Bluebeard's wife",
+      "FEARNOT (Between you, me and the lamppost)",
+      "Flash Forward",
+      "Fire in the belly"
+    ]
+  },
+  {
+    "title": "Guardian",
+    "type": "OST Single (BASTIONS OST Part.4)",
+    "release_date": "2023-06-09",
+    "songs": [
+      "Guardian",
+      "Guardian (Instrumental)"
+    ]
+  },
+  {
+    "title": "Eve, Psyche & The Bluebeard's Wife (feat. BIBI, Camo, Mirani)",
+    "type": "Single",
+    "release_date": "2023-06-27",
+    "songs": [
+      "Eve, Psyche & The Bluebeard's Wife (feat. BIBI, Camo, Mirani)"
+    ]
+  },
+  {
+    "title": "Eve, Psyche & The Bluebeard's Wife (English Ver.)",
+    "type": "English Single",
+    "release_date": "2023-07-06",
+    "songs": [
+      "Eve, Psyche & The Bluebeard's Wife (English Ver.)"
+    ]
+  },
+  {
+    "title": "UNFORGIVEN (Japanese ver.)",
+    "type": "2nd Japanese Single Album",
+    "release_date": "2023-08-22",
+    "songs": [
+      "UNFORGIVEN -Japanese ver.- (ft.Nile Rodgers, Ado)",
+      "ANTIFRAGILE (Japanese ver.)",
+      "ジュエリー (Prod.imase)"
+    ]
+  },
+  {
+    "title": "Perfect Night",
+    "type": "Single",
+    "release_date": "2023-10-27",
+    "songs": [
+      "Perfect Night"
+    ]
+  },
+  {
+    "title": "Perfect Night (Remix)",
+    "type": "Remix Single",
+    "release_date": "2023-10-30",
+    "songs": [
+      "Perfect Night (Speed Up ver.)",
+      "Perfect Night (Slowed + Reverb ver.)"
+    ]
+  },
+  {
+    "title": "ドレスコード (Dresscode) (Prod. imase)",
+    "type": "Japanese OST Single",
+    "release_date": "2023-11-19",
+    "songs": [
+      "ドレスコード (Dresscode) (Prod. imase)"
+    ]
+  },
+  {
+    "title": "Perfect Night (Holiday Remix)",
+    "type": "Remix Single",
+    "release_date": "2023-11-23",
+    "songs": [
+      "Perfect Night (Holiday Remix)"
+    ]
+  },
+  {
+    "title": "EASY",
+    "type": "3rd Mini Album",
+    "release_date": "2024-02-19",
+    "songs": [
+      "Good Bones",
+      "EASY",
+      "Swan Song",
+      "Snart",
+      "We got so much"
+    ]
+  },
+  {
+    "title": "EASY (English Ver.)",
+    "type": "English Single",
+    "release_date": "2024-02-23",
+    "songs": [
+      "EASY (English Ver.)",
+      "EASY (English Ver.) (Speed Up ver.)",
+      "EASY (English Ver.) (Slowed + Reverb ver.)"
+    ]
+  },
+  {
+    "title": "EASY (Remixes)",
+    "type": "Remixes",
+    "release_date": "2024-02-28",
+    "songs": [
+      "EASY",
+      "EASY (Pluggnb Remix)",
+      "EASY (Drum & Bass Remix)",
+      "EASY (Sped Up ver.)",
+      "EASY (Slowed + Reverb ver.)",
+      "EASY (Instrumental)"
+    ]
+  },
+  {
+    "title": "Smart (Remixes)",
+    "type": "Remixes",
+    "release_date": "2024-03-22",
+    "songs": [
+      "Smart",
+      "Smart (English ver.)",
+      "Smart (Smartest Remix)",
+      "Smart (Chill Remix)",
+      "Smart (Miami Bass Remix)",
+      "Smart (Festival House Remix)",
+      "Smart (Sped Up ver.)",
+      "Smart (Slowed + Reverb ver.)",
+      "Smart (Instrumental)"
+    ]
+  },
+  {
+    "title": "Perfect Night (For FEARNOT)",
+    "type": "4th English Remix Single",
+    "release_date": "2024-08-04",
+    "songs": [
+      "Perfect Night (for FEARNOT)",
+      "Perfect Night"
+    ]
+  },
+  {
+    "title": "CRAZY",
+    "type": "4th Mini Album",
+    "release_date": "2024-08-30",
+    "songs": [
+      "Chasing Lightning",
+      "CRAZY",
+      "Pierrot",
+      "1-800-hot-n-fun",
+      "미치지 못하는 이유"
+    ]
+  },
+  {
+    "title": "CRAZY (Party Remixes 1)",
+    "type": "1st Remix Album",
+    "release_date": "2024-09-02",
+    "songs": [
+      "CRAZY",
+      "CRAZY (English ver.)",
+      "CRAZY (Vogue Remix) (feat. Dashaun Wesley)",
+      "CRAZY (Bounce Up Remix)",
+      "CRAZY (Dance Remix)",
+      "CRAZY (Sped Up ver.)",
+      "CRAZY (Slowed + Reverb ver.)",
+      "CRAZY (Inst.)"
+    ]
+  },
+  {
+    "title": "CRAZY (Feat. PinkPantheress)",
+    "type": "3rd Remix Single",
+    "release_date": "2024-09-04",
+    "songs": [
+      "CRAZY",
+      "CRAZY (feat. PinkPantheress)"
+    ]
+  },
+  {
+    "title": "CRAZY (David Guetta Remix)",
+    "type": "4th Remix Single",
+    "release_date": "2024-09-09",
+    "songs": [
+      "CRAZY (David Guetta Remix)",
+      "CRAZY",
+      "CRAZY (David Guetta Remix) (Extended ver.)"
+    ]
+  },
+  {
+    "title": "CRAZY (Party Remixes 2)",
+    "type": "2nd Remix Album",
+    "release_date": "2024-09-12",
+    "songs": [
+      "CRAZY",
+      "CRAZY (EBM Remix)",
+      "CRAZY (Super Crazy Remix)",
+      "CRAZY (Vamos Remix)",
+      "CRAZY (Mash-up ver.) (feat. PinkPantheress, Dashaun Wesley)"
+    ]
+  },
+  {
+    "title": "Star Signs",
+    "type": "Japanese Pre-Release Single",
+    "release_date": "2024-11-13",
+    "songs": [
+      "Star Signs"
+    ]
+  },
+  {
+    "title": "CRAZY (Japenese ver.)",
+    "type": "3rd Japanese Single",
+    "release_date": "2024-12-11",
+    "songs": [
+      "CRAZY (Japanese ver.)",
+      "EASY (Japanese ver.)",
+      "Star Signs"
+    ]
+  },
+  {
+    "title": "CRAZY (Japanese ver.) (Party Remixes)",
+    "type": "Remixes Single",
+    "release_date": "2024-12-18",
+    "songs": [
+      "CRAZY (Japanese ver.)",
+      "CRAZY (Japanese ver.) (feat.JP THE WAVY)",
+      "CRAZY (Japanese ver.) (Chaki Zulu Remix)"
+    ]
+  },
+  {
+    "title": "HOT",
+    "type": "5th Mini Album",
+    "release_date": "2025-03-14",
+    "songs": [
+      "Born Fire",
+      "HOT",
+      "Come Over",
+      "Ash",
+      "So Cynical (Badum)"
+    ]
+  },
+  {
+    "title": "HOT (English ver.)",
+    "type": "Single",
+    "release_date": "2025-03-17",
+    "songs": [
+      "HOT (English ver.)",
+      "HOT",
+      "HOT (Sped Up ver.)",
+      "HOT (Slowed + Reverb ver.)",
+      "HOT (Inst.)"
+    ]
+  },
+  {
+    "title": "HOT (LE SSERAFIM Package)",
+    "type": "Package Album",
+    "release_date": "2025-03-18",
+    "songs": [
+      "HOT",
+      "HOT (KIM CHAEWON ver.)",
+      "HOT (SAKURA ver.)",
+      "HOT (HUH YUNJIN ver.)",
+      "HOT (KAZUHA ver.)",
+      "HOT (HONG EUNCHAE ver.)"
+    ]
+  }
+]
